371d0a56a87721d06af72d3b7e180f7d575ac003
Samarth Srivastava <samarthsrivastava897@gmail.com>
2025-08-08 00:11:44 +0530

    fix2

--- encaps/src/App.jsx
+++ encaps/src/App.jsx
@@ -5,56 +5,39 @@ import WriteLetter from './components/WriteLetter'
 import ScheduledLetters from './components/ScheduledLetters'
 import About from './components/About'
 
-import {config} from "./config"
-import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
-import { WagmiProvider } from 'wagmi'
-
 import './App.css'
 
 function App() {
   const [currentPage, setCurrentPage] = useState('hero');
 
-  const navigateToWriteLetter = () => {
-    setCurrentPage('writeLetter');
-  };
-
-  const navigateToScheduledLetters = () => {
-    setCurrentPage('scheduledLetters');
-  };
-
-  const navigateToHero = () => {
-    setCurrentPage('hero');
-  };
-
-  const navigateToAbout = () => {
-    setCurrentPage('about');
-  };
-  const client=new QueryClient();
+  const navigateToWriteLetter = () => setCurrentPage('writeLetter');
+  const navigateToScheduledLetters = () => setCurrentPage('scheduledLetters');
+  const navigateToHero = () => setCurrentPage('hero');
+  const navigateToAbout = () => setCurrentPage('about');
 
   return (
-    <WagmiProvider config={config} >
-    <QueryClientProvider client={client}>
-    <div>
     <div className="App">
       <Navbar onNavigate={navigateToHero} />
       {currentPage === 'hero' ? (
-        <Hero onWriteLetter={navigateToWriteLetter} onScheduledLetters={navigateToScheduledLetters} onAbout={navigateToAbout} />
+        <Hero
+          onWriteLetter={navigateToWriteLetter}
+          onScheduledLetters={navigateToScheduledLetters}
+          onAbout={navigateToAbout}
+        />
       ) : currentPage === 'writeLetter' ? (
         <WriteLetter onViewScheduledLetters={navigateToScheduledLetters} />
       ) : currentPage === 'about' ? (
-        <About onNavigate={(page) => {
-          if (page === 'hero') navigateToHero();
-          else if (page === 'writeLetter') navigateToWriteLetter();
-        }} />
+        <About
+          onNavigate={(page) => {
+            if (page === 'hero') navigateToHero();
+            else if (page === 'writeLetter') navigateToWriteLetter();
+          }}
+        />
       ) : (
         <ScheduledLetters onWriteLetter={navigateToWriteLetter} />
       )}
     </div>
-    </div>
-    </QueryClientProvider>
-    </WagmiProvider>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- encaps/src/config.js
+++ encaps/src/config.js
@@ -5,7 +5,7 @@ import { injected } from 'wagmi/connectors'
 
 export const config = createConfig({
   chains: [sepolia],
-  connectors:[],
+  connectors: [],
   transports: {
     [sepolia.id]: http("https://eth-sepolia.g.alchemy.com/v2/MmeeikFI32X3D30PvPBHhRZvYx2Ewikf"),
   },
